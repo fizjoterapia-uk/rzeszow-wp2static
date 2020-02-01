e28dc3f81aea7e9b88b5c1d954d7b9736b1c777a
Bart Prokop <prokop.bart@gmail.com>
2020-02-01 20:09:08 +0000

    WP2Static export as is...

---
+++ wp-content/themes/upside-lite/js/probars.js
@@ -0,0 +1,4 @@
+function animateProgressBar(){jQuery('.pro-bar').each(function(i,elem){var elem=jQuery(this),percent=elem.attr('data-pro-bar-percent'),delay=elem.attr('data-pro-bar-delay');if(!elem.hasClass('animated'))
+elem.css({'width':'0%'});if(elem.visible(true)){setTimeout(function(){elem.animate({'width':percent+'%'},2000,'easeInOutExpo').addClass('animated');},delay);}});}
+jQuery(document).ready(function(){animateProgressBar();});jQuery(window).resize(function(){animateProgressBar();});jQuery(window).scroll(function(){animateProgressBar();if(jQuery(window).scrollTop()+jQuery(window).height()==jQuery(document).height())
+animateProgressBar();});
